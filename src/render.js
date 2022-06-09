1b46ce9f9e17e2357884c4c47ca538e06356bf09
MSFTserver <MSFTserver@gmail.com>
2022-06-09 09:24:09 -0700

    format

--- src/render.js
+++ src/render.js
@@ -58,153 +58,153 @@ function init(fileExt, fileData) {
         document.getElementById("loading").style.display = "none";
 
         document.getElementById("container2").style.display = "none";
-        console.log("EXT: ",fileExt)
+        console.log("EXT: ", fileExt)
         alert(STRING_ERROR);
     }
-    
+
     renderer = new THREE.WebGLRenderer({ antialias: false });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    
-        try {
-            let contents = fileData;
-            console.log("LOAD: ",contents);
-            if (fileExt === "obj") {
-                let object = new THREE.OBJLoader().parse(contents);
-                let sceneConverter = new THREE.Scene();
-                sceneConverter.add(object);
-                let exporter = new THREE.STLExporter();
-                contents = exporter.parse(sceneConverter);
-            }
-            else if (fileExt === "3ds") {
-                let object = new THREE.TDSLoader().parse(contents);
-                let sceneConverter = new THREE.Scene();
-                sceneConverter.add(object);
-                let exporter = new THREE.STLExporter();
-                contents = exporter.parse(sceneConverter);
-            }
 
-            let geometry = new THREE.STLLoader().parse(contents);
-            geometry.computeFaceNormals();
-            geometry.computeVertexNormals();
-            geometry.center();
+    try {
+        let contents = fileData;
+        console.log("LOAD: ", contents);
+        if (fileExt === "obj") {
+            let object = new THREE.OBJLoader().parse(contents);
+            let sceneConverter = new THREE.Scene();
+            sceneConverter.add(object);
+            let exporter = new THREE.STLExporter();
+            contents = exporter.parse(sceneConverter);
+        }
+        else if (fileExt === "3ds") {
+            let object = new THREE.TDSLoader().parse(contents);
+            let sceneConverter = new THREE.Scene();
+            sceneConverter.add(object);
+            let exporter = new THREE.STLExporter();
+            contents = exporter.parse(sceneConverter);
+        }
 
-            let material = new THREE.MeshPhongMaterial({ color: 0x00FF00, emissive:0x000000, emissiveIntensity:2});
-            mesh = new THREE.Mesh(geometry, material);
+        let geometry = new THREE.STLLoader().parse(contents);
+        geometry.computeFaceNormals();
+        geometry.computeVertexNormals();
+        geometry.center();
 
-            // CALCULATING THE VOLUME
-            vol = 0;
+        let material = new THREE.MeshPhongMaterial({ color: 0x00FF00, emissive: 0x000000, emissiveIntensity: 2 });
+        mesh = new THREE.Mesh(geometry, material);
 
-            mesh.traverse(function (child) {
-                if (child instanceof THREE.Mesh) {
-                    let positions = child.geometry.getAttribute("position").array;
-                    for (let i = 0; i < positions.length; i += 9) {
-                        let t1 = {};
-                        t1.x = positions[i + 0];
-                        t1.y = positions[i + 1];
-                        t1.z = positions[i + 2];
+        // CALCULATING THE VOLUME
+        vol = 0;
 
-                        let t2 = {};
-                        t2.x = positions[i + 3];
-                        t2.y = positions[i + 4];
-                        t2.z = positions[i + 5];
+        mesh.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                let positions = child.geometry.getAttribute("position").array;
+                for (let i = 0; i < positions.length; i += 9) {
+                    let t1 = {};
+                    t1.x = positions[i + 0];
+                    t1.y = positions[i + 1];
+                    t1.z = positions[i + 2];
 
-                        let t3 = {};
-                        t3.x = positions[i + 6];
-                        t3.y = positions[i + 7];
-                        t3.z = positions[i + 8];
+                    let t2 = {};
+                    t2.x = positions[i + 3];
+                    t2.y = positions[i + 4];
+                    t2.z = positions[i + 5];
 
-                        vol += signedVolumeOfTriangle(t1, t2, t3);
-                    }
+                    let t3 = {};
+                    t3.x = positions[i + 6];
+                    t3.y = positions[i + 7];
+                    t3.z = positions[i + 8];
+
+                    vol += signedVolumeOfTriangle(t1, t2, t3);
                 }
-            });
-
-            let box = new THREE.Box3().setFromObject(mesh);
-
-            height = box.max.z - box.min.z;
-            width = box.max.x - box.min.x;
-            depth = box.max.y - box.min.y;
-
-            heightFinal = height / 10; heightFinal = heightFinal.toFixed(2);
-            widthFinal = width / 10; widthFinal = widthFinal.toFixed(2);
-            depthFinal = depth / 10; depthFinal = depthFinal.toFixed(2);
-            let volumeFinal = vol / 1000; volumeFinal = volumeFinal.toFixed(2);
-            let weightFinal = volumeFinal * density; weightFinal = weightFinal.toFixed(2);
-
-            let filament_length = parseFloat((vol / (filament_diameter / 2) ^ 2 / Math.PI) * 2 / 10).toFixed(2);
-            filament_length = parseFloat(filament_length).toFixed(0);
-
-            let hours = Math.floor((filament_length / printing_speed) / 60);
-            hours = parseFloat(hours).toFixed(0);
-
-            let minutes = (filament_length / printing_speed) % 60;
-            minutes = parseFloat(minutes).toFixed(0);
-
-            if (minutes == 0) { minutes = 1; }
-
-            let finalCost = weightFinal * filament_cost / 1000;
-            finalCost = parseFloat(finalCost).toFixed(2);
-
-            document.getElementById("container2").style.display = "block";
-            document.getElementById("densityValue").innerHTML = density;
-            document.getElementById("weightValue").innerHTML = weightFinal;
-            document.getElementById("volumeValue").innerHTML = volumeFinal;
-            document.getElementById("widthValue").innerHTML = widthFinal;
-            document.getElementById("depthValue").innerHTML = depthFinal;
-            document.getElementById("heightValue").innerHTML = heightFinal;
-            document.getElementById("costKilogramValue").innerHTML = filament_cost;
-            document.getElementById("costValue").innerHTML = finalCost;
-            document.getElementById("diameterValue").innerHTML = filament_diameter;
-            document.getElementById("speedValue").innerHTML = printing_speed;
-            document.getElementById("lengthValue").innerHTML = filament_length;
-            document.getElementById("hoursValue").innerHTML = hours;
-            document.getElementById("minutesValue").innerHTML = minutes;
-
-            let distance;
-
-            if (height > width && height > depth) {
-                distance = height * 2;
             }
-            else if (width > height && width > depth) {
-                distance = width * 2;
-            }
-            else if (depth > height && depth > width) {
-                distance = depth * 2;
-            }
-            else {
-                distance = depth * 4;
-            }
+        });
 
-            camera.position.set(0, -distance, 0);
+        let box = new THREE.Box3().setFromObject(mesh);
 
-            let x = distance + 200;
-            let y = distance + 200;
-            let division_x = Math.floor(x / 10);
-            let division_y = Math.floor(y / 10);
+        height = box.max.z - box.min.z;
+        width = box.max.x - box.min.x;
+        depth = box.max.y - box.min.y;
 
-            // AN ALTERNATIVE FOR MOVING THE OBJECT USING THE MOUSE WITHIN THE RENDERER
-            controls = new THREE.OrbitControls(camera, renderer.domElement);
-            // controls = new THREE.OrbitControls(camera);
-            function animateSpin(){
-                controls.update();
-                requestAnimationFrame ( animateSpin );  
-                renderer.render (scene, camera);
-            }
+        heightFinal = height / 10; heightFinal = heightFinal.toFixed(2);
+        widthFinal = width / 10; widthFinal = widthFinal.toFixed(2);
+        depthFinal = depth / 10; depthFinal = depthFinal.toFixed(2);
+        let volumeFinal = vol / 1000; volumeFinal = volumeFinal.toFixed(2);
+        let weightFinal = volumeFinal * density; weightFinal = weightFinal.toFixed(2);
+
+        let filament_length = parseFloat((vol / (filament_diameter / 2) ^ 2 / Math.PI) * 2 / 10).toFixed(2);
+        filament_length = parseFloat(filament_length).toFixed(0);
+
+        let hours = Math.floor((filament_length / printing_speed) / 60);
+        hours = parseFloat(hours).toFixed(0);
+
+        let minutes = (filament_length / printing_speed) % 60;
+        minutes = parseFloat(minutes).toFixed(0);
+
+        if (minutes == 0) { minutes = 1; }
+
+        let finalCost = weightFinal * filament_cost / 1000;
+        finalCost = parseFloat(finalCost).toFixed(2);
+
+        document.getElementById("container2").style.display = "block";
+        document.getElementById("densityValue").innerHTML = density;
+        document.getElementById("weightValue").innerHTML = weightFinal;
+        document.getElementById("volumeValue").innerHTML = volumeFinal;
+        document.getElementById("widthValue").innerHTML = widthFinal;
+        document.getElementById("depthValue").innerHTML = depthFinal;
+        document.getElementById("heightValue").innerHTML = heightFinal;
+        document.getElementById("costKilogramValue").innerHTML = filament_cost;
+        document.getElementById("costValue").innerHTML = finalCost;
+        document.getElementById("diameterValue").innerHTML = filament_diameter;
+        document.getElementById("speedValue").innerHTML = printing_speed;
+        document.getElementById("lengthValue").innerHTML = filament_length;
+        document.getElementById("hoursValue").innerHTML = hours;
+        document.getElementById("minutesValue").innerHTML = minutes;
+
+        let distance;
+
+        if (height > width && height > depth) {
+            distance = height * 2;
+        }
+        else if (width > height && width > depth) {
+            distance = width * 2;
+        }
+        else if (depth > height && depth > width) {
+            distance = depth * 2;
+        }
+        else {
+            distance = depth * 4;
+        }
+
+        camera.position.set(0, -distance, 0);
+
+        let x = distance + 200;
+        let y = distance + 200;
+        let division_x = Math.floor(x / 10);
+        let division_y = Math.floor(y / 10);
+
+        // AN ALTERNATIVE FOR MOVING THE OBJECT USING THE MOUSE WITHIN THE RENDERER
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        // controls = new THREE.OrbitControls(camera);
+        function animateSpin() {
             controls.update();
-            console.log(controls);
-            scene.add(mesh);
-            controls.autoRotate = true;
-            animateSpin();
-            // HIDDING THE LOADING SPLASH
-            document.getElementById("loading").style.display = "none";
+            requestAnimationFrame(animateSpin);
+            renderer.render(scene, camera);
         }
-        catch (err) {
-            // HIDDING THE LOADING SPLASH
-            document.getElementById("loading").style.display = "none";
+        controls.update();
+        console.log(controls);
+        scene.add(mesh);
+        controls.autoRotate = true;
+        animateSpin();
+        // HIDDING THE LOADING SPLASH
+        document.getElementById("loading").style.display = "none";
+    }
+    catch (err) {
+        // HIDDING THE LOADING SPLASH
+        document.getElementById("loading").style.display = "none";
 
-            document.getElementById("container2").style.display = "none";
-            console.log(err)
-            alert(STRING_ERROR);
-        }
+        document.getElementById("container2").style.display = "none";
+        console.log(err)
+        alert(STRING_ERROR);
+    }
 
     light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
     light.position.set(0, 1, 0);
@@ -362,8 +362,6 @@ window.onload = async function () {
     let data = await fetch(
         `${print2aApiEndpoint}/GetFile?fileLocation=print2a/${filePath}`,
     )
-    console.log(data)
     let fileData = await data.arrayBuffer()
-    console.log(fileData)
     init(fileExt, fileData);
 }
